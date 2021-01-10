ff4b978a4ec03422b40c5bd3bfcc17f066011826
patrykkrajnik <patrykkrajnik@icloud.com>
2021-01-10 18:57:01 +0100

    MusicPlayerViewController init

---
+++ MusicAll/MusicAll/Music Player/MusicPlayerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerViewController.swift
+//  MusicAll
+//
+//  Created by Patryk Krajnik on 10/01/2021.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
