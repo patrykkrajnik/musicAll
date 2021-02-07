a4d26b186c3df7112ab22595dd7f3e77104fe782
patrykkrajnik <patrykkrajnik@icloud.com>
2021-02-07 20:20:57 +0100

    SongModel created

---
+++ MusicAll/MusicAll/Model/SongModel.swift
@@ -0,0 +1,13 @@
+//
+//  SongModel.swift
+//  MusicAll
+//
+//  Created by Patryk Krajnik on 07/02/2021.
+//
+
+import Foundation
+
+struct SongModel: Codable {
+    var songName: String
+    var songURL: String
+}
