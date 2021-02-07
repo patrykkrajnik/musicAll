279309cefdbd6d1b691dff2183f89e33aed6caa6
patrykkrajnik <patrykkrajnik@icloud.com>
2021-02-07 20:26:00 +0100

    Parsing data from JSON file done, SearchBar with filtering content added, refreshing TableView added

--- MusicAll/MusicAll/Music List/MusicListViewController.swift
+++ MusicAll/MusicAll/Music List/MusicListViewController.swift
@@ -7,20 +7,50 @@
 
 import UIKit
 
-class MusicListViewController: UITableViewController {
+class MusicListViewController: UITableViewController, UISearchBarDelegate {
     
     @IBOutlet var musicList: UITableView!
     
+    lazy var searchBar: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search..."
+        searchController.searchBar.sizeToFit()
+        searchController.searchBar.searchBarStyle = .prominent
+        searchController.searchBar.delegate = self
+        
+        return searchController
+    }()
+    
+    var songs = [SongModel]()
+    var filteredSongs = [SongModel]()
+    
+    final let apiURL = "Here is a place for URL to your API"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchBar
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(refreshContent(sender:)), for: .valueChanged)
+        
         configureTableView()
+        prepareToParse()
+    }
+    
+    @objc func refreshContent(sender: AnyObject) {
+        prepareToParse()
+        refreshControl?.endRefreshing()
     }
     
     func configureTableView() {
         setTableViewDelegates()
         self.title = "Library"
         musicList.rowHeight = 100
-        navigationController?.navigationBar.prefersLargeTitles = true
+        musicList.tableFooterView = UITableView()
     }
     
     func setTableViewDelegates() {
@@ -28,12 +58,103 @@ class MusicListViewController: UITableViewController {
         musicList.dataSource = self
     }
     
+    func prepareToParse() {
+        if let url = URL(string: apiURL) {
+            guard let data = try? Data(contentsOf: url) else {
+                print("Not a valid URL")
+                return
+            }
+            parseJsonFromURL(json: data)
+        } else {
+            parseLocalJson()
+        }
+    }
+    
+    func parseLocalJson() {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        guard
+            let localJson = Bundle.main.url(forResource: "localMusic", withExtension: "json"),
+            let data = try? Data(contentsOf: localJson),
+            let jsonSongs = try? decoder.decode([SongModel].self, from: data)
+        else {
+            print("JSON file could not be found")
+            return
+        }
+        songs = jsonSongs
+    }
+    
+    func parseJsonFromURL(json: Data) {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        if let jsonSongs = try? decoder.decode([SongModel].self, from: json) {
+            songs = jsonSongs
+            musicList.reloadData()
+        }
+    }
+    
+    func filterContentForSearchText(searchText: String) {
+        filteredSongs = songs.filter({(song: SongModel) -> Bool in
+            if isSearchBarEmpty() {
+                return true
+            } else {
+                return song.songName.lowercased().contains(searchText.lowercased())
+            }
+        })
+        musicList.reloadData()
+    }
+    
+    func isSearchBarEmpty() -> Bool {
+        return searchBar.searchBar.text?.isEmpty ?? true
+    }
+    
+    func isFiltering() -> Bool {
+        return searchBar.isActive && (!isSearchBarEmpty())
+    }
+    
+    func getSongTitle(songName: String) -> String {
+        let songFullName = songName.components(separatedBy: "-")
+        let songTitle = songFullName[1]
+        let trimmedSongTitle = songTitle.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        return trimmedSongTitle
+    }
+    
+    func getSongArtist(songName: String) -> String {
+        let songFullName = songName.components(separatedBy: "-")
+        let songArtist = songFullName[0]
+        let trimmedSongArtist = songArtist.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        return trimmedSongArtist
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if isFiltering() { return filteredSongs.count }
+        return songs.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MusicCell") as? MusicListCell else {return UITableViewCell()}
+        
+        let currentSongs: SongModel
+        
+        if isFiltering() {
+            currentSongs = filteredSongs[indexPath.row]
+        } else {
+            currentSongs = songs[indexPath.row]
+        }
+        
+        cell.songArtist.text = getSongArtist(songName: currentSongs.songName)
+        cell.songTitle.text = getSongTitle(songName: currentSongs.songName)
+        return cell
+    }
+}
+
+extension MusicListViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
     }
 }
 
