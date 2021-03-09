3955102fc2b6660f90efea0d96317b6ec26c3389
patrykkrajnik <patrykkrajnik@icloud.com>
2021-03-09 21:03:50 +0100

    Referencing Outlets for MusicPlayerViewController implemented

--- MusicAll/MusicAll/Music Player/MusicPlayerViewController.swift
+++ MusicAll/MusicAll/Music Player/MusicPlayerViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MusicPlayerViewController: UIViewController {
-
+    
+    @IBOutlet weak var songTitleLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var totalTimeLabel: UILabel!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var progressSlider: UISlider!
+    
+    @IBOutlet weak var songArtworkView: UIImageView!
+    
+    @IBOutlet weak var playPauseButtonView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    
+    private var timer: Timer!
+    static var player: AVPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
 }
